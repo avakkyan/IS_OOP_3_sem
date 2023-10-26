91d75fe6b761d865e9c36e0f56dbaef3e0a60ac6
avakkyan <avakkyan@gmail.com>
2023-10-26 22:36:20 +0300

    feat: created Repository

---
+++ src/Lab2/Services/Repository.cs
@@ -0,0 +1,18 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab2.DataBase;
+
+namespace Itmo.ObjectOrientedProgramming.Lab2.Services;
+
+public class Repository
+{
+    private ComponentsDatabase _datailStorage = new ComponentsDatabase();
+
+    public void UpdateDataile(IComputerDetail detail)
+    {
+        _datailStorage.Update(detail);
+    }
+
+    public IComputerDetail? FindMyDetail(string detailName)
+    {
+        return _datailStorage.FindDetail(detailName);
+    }
+}
