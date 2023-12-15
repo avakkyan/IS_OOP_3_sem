8a0f1e3bcf964c787b35a540f0e103153380dfda
avakkyan <avakkyan@gmail.com>
2023-12-16 02:38:12 +0300

    feat: tests

---
+++ tests/Lab5.Tests/Mocks/UserDataBase.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Workshop5.Application.Users;
+
+namespace Itmo.ObjectOrientedProgramming.Lab5.Tests.Mocks;
+
+public class UserDataBase
+{
+    private readonly IList<UserTable> _users = new List<UserTable>();
+
+    public UserDataBase()
+    {
+        string username = "karina";
+        string password = "123";
+        _users.Add(new UserTable(username, password, 0));
+    }
+
+    public void ChancgeBalance(string username, string password, decimal value)
+    {
+        foreach (UserTable data in _users)
+        {
+            if (data.Name == username && data.Password == password)
+            {
+                data.ChangeBalance(value);
+                return;
+            }
+        }
+    }
+
+    public decimal? GetBalance(string? username, string password)
+    {
+        decimal? balance;
+        foreach (UserTable data in _users)
+        {
+            if (data.Name == username && data.Password == password)
+            {
+                balance = data.Balance;
+                return balance;
+            }
+        }
+
+        return null;
+    }
+
+    public void AddUser(string name, string password, int balance)
+    {
+        _users.Add(new UserTable(name, password, balance));
+    }
+}
---
+++ tests/Lab5.Tests/Mocks/UserRepositoryMock.cs
@@ -0,0 +1,23 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab5.Tests.Mocks;
+
+namespace Lab5.Infastructure.DataAccess.Repositories;
+
+public class UserRepositoryMock
+{
+    private readonly UserDataBase userMock = new UserDataBase();
+    public decimal? CheckAccountBalance(string? name, string password)
+    {
+        decimal? value = userMock.GetBalance(name, password);
+        return value;
+    }
+
+    public void CreateBankAccount(string name, string password)
+    {
+        userMock.AddUser(name, password, 0);
+    }
+
+    public void RefillMoneyOnAccount(string username, string password, decimal amount)
+    {
+        userMock.ChancgeBalance(username, password, amount);
+    }
+}
